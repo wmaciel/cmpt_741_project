9232be1e9b1cee12d14381bfdfdcab685b9c99bf
Walther Maciel <walthermaciel@gmail.com>
2015-12-03 14:54:24 -0800

    Frequent Pattern Mining from clusters
    Finished script that call frequent pattern mining from each cluster and
    them concatenates the results.

--- src/frequent_itemset_clusters.py
+++ src/frequent_itemset_clusters.py
@@ -1,18 +1,29 @@
 __author__ = 'walthermaciel'
 
 import sys
-from frequent_itemset import compute_frequent_itemsets
+from frequent_itemsets import compute_frequent_itemsets
 
-def main(cluster_folder, output_file):
-    fp_tmp = []
+def main(cluster_folder, output_file, min_sup, assoc_min_sup, assoc_min_conf ):
+    print 'Opening output file:', output_file, '...'
+    fp_out = open(output_file, 'w')
+
     for i in xrange(4):
         c_str = cluster_folder + '/c' + str(i) + '.txt'
         tmp_str = cluster_folder + '/out_c' + str(i) + '.txt'
-        compute_frequent_itemsets(c_str, tmp_str)
-        fp_tmp.append(open(tmp_str, 'r'))
+        print 'Computing frequent itemsets on cluster', i, '...'
+        compute_frequent_itemsets(c_str, tmp_str, int(min_sup), int(assoc_min_sup), float(assoc_min_conf))
+        print 'Opening temp file...',
+        fp_tmp = open(tmp_str, 'r')
+        print 'Apending to output file...',
+        fp_out.write(fp_tmp.read())
+        print 'Done!'
+        fp_tmp.close()
+
+    print 'Closing output file...'
+    fp_out.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print 'parameters: cluster_folder , output file
+        print 'parameters: cluster_folder , output file, min_sup, assoc_min_sup, assoc_min_conf'
     else:
-        main(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
